7bf95d11df4a4ccc15105cba46de9b1fe4f46683
Jose Fabrizio <josefabriziomanco@gmail.com>
2018-11-20 07:30:20 -0500

    E01

---
+++ e01.py
@@ -0,0 +1,10 @@
+# Entrada
+eje_x = int(input("Ingrese la primera cordenada en el eje x\n"))
+eje_y = int(input("Ingrese la segunda coordenada en el eje y\n"))
+
+coordenada = [eje_x,eje_y]
+
+distancia = (eje_x - 0)/(eje_y - 0)
+
+print("Cordenada: ",coordenada)
+print("La distancia de la coordenada [0,0] a la coordenada ingresada es: ", distancia)
